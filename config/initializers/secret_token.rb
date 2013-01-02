de7452daf55e4135f0beba3972aede85a8ee4d2d
Zhang Jian <zjloveztt@gmail.com>
2013-01-02 20:35:24 +0800

    i

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scrum::Application.config.secret_token = 'b175025c55d908dd51c1c64454f9b0703314fcb6b38129cfa6a6f9d5d3beee8ef236f112150aa0596dda398ef52a4e62e2438878d7f8d6b5051968ef41637c3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Scrum::Application.config.session_store :cookie_store, key: '_scrum_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Scrum::Application.config.session_store :active_record_store
