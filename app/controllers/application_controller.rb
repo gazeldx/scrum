0cc22dee6de6e54caf75f7bb5891662f24c4f973
lanejianzhang <zhangjian-sz@shinetechchina.com>
2013-01-22 11:54:50 +0800

    d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+  private
+  def authenticate
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == "jim" && password == "shinetech"
+    end
+  end
 end
