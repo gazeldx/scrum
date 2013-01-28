ed77bc568bf5fe243a2a67054b83e6cb385f1fe8
lanejianzhang <zhangjian-sz@shinetechchina.com>
2013-01-28 11:26:33 +0800

    add handle sub domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate
+  before_filter :authenticate, :handle_sub_domain
+
+  def handle_sub_domain
+    redirect_to "http://shinescrum.com" if request.subdomain.present?
+  end
+
   private
   def authenticate
     authenticate_or_request_with_http_basic do |user_name, password|
